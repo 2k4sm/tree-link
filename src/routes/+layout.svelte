30d3dd09aff2ee5270e745dd8c45a2b72a192673
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-12-16 13:16:42 +0530

    adds multi step form layouts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-   import "../app.css"
-   interface Props {
-      children?: import('svelte').Snippet;
-   }
-
-   let { children }: Props = $props();
+	import '../app.css';
+	let { children } = $props();
 </script>
 
-<div class="min-h-screen flex flex-col">
-    {@render children?.()}
-</div>
+{@render children()}
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
-    import type { Snippet } from 'svelte';
-    import type { LayoutData } from './login/$types';
-
-    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+  import { page } from "$app/stores";
+  
+  let {children} = $props();
 </script>
 
-{@render children()}
+<nav class="flex justify-center my-6">
+  <ul class="steps">
+    <a href="/login" class="step step-primary">Sign In</a>
+    <a
+      href="/login/username"
+      class="step"
+      class:step-primary={$page.route.id?.match(/username|photo/g)}>
+      Choose Username
+    </a>
+    <a
+      href="/login/photo"
+      class="step"
+      class:step-primary={$page.route.id?.includes("photo")}>
+      Upload Photo
+    </a>
+  </ul>
+</nav>
+
+<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+  <div class="card-body items-center text-center">
+    {@render children()}
+  </div>
+</main>
