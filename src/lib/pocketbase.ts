553babc4e7c5236014f117ca78394771cab1ae1b
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-12-25 03:09:45 +0530

    impr: adds oauth login using google

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,12 +1,9 @@
-import * as dotenv from "dotenv";
+import { env } from "$env/dynamic/public";
 import PocketBase from "pocketbase";
 
-dotenv.config();
-
 function initDb(): PocketBase {
-  const uri = process.env.PB_URI;
-
-  const pb_client = new PocketBase(uri);
+  const url = env.PUBLIC_PB_URI;
+  const pb_client = new PocketBase(url);
 
   pb_client.autoCancellation(false);
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,3 +1,13 @@
 <script lang="ts">
+    import pb from "$lib/pocketbase"
+    async function signInWithGoogle() {
+        const authData = await pb.collection('users').authWithOAuth2({ provider: 'google' });
+        console.log(authData)
+        console.log(pb.authStore.record);
+        pb.authStore.clear();
+    }
     
 </script>
+
+<div>
+<button class="btn btn-primary" onclick={signInWithGoogle}>Sign in with Google</button></div>
